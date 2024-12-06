b3878981a528759bc76206f36f3b24793fe75c95
Johnson Jaurigue <johnsonjaurigue2@gmail.com>
2024-12-06 15:53:44 +0800

    .

--- src/main.rs
+++ src/main.rs
@@ -6,83 +6,44 @@ use tokio::net::{TcpListener, TcpStream};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use log::warn;
 
-use http::{HeaderMap, HeaderName, HeaderValue, Request, Response, StatusCode, Version};
+use http::Response;
 mod models;
-
+mod handlers;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>>{
-	
 	dotenv().ok();
-	let address = env::var("ADDRESS");
-	let port = env::var("PORT");
 	
-	if address.is_ok() && port.is_ok() {
-		let address_val = address.unwrap();
-		let port_val = port.unwrap();
-		println!("Attempting to connect to {address}:{port}",address=&address_val, port=&port_val );
-		let listener = TcpListener::bind(format!("{address}:{port}",address=address_val, port=port_val)).await?;
+	let mut database_uri = String::new();
+	let mut listening_address = String::new();
+	let mut listening_port = String::new();
+	
+	match env::var("STATE") {
 		
-		println!("Now listening!");
-		loop {
-			let (stream, _addr) = listener.accept().await?;
-			
-			tokio::spawn(async move {
-				let _listen_result = listen(stream).await;
-			});
+		Ok(state) if state.to_string() == "DEV"=> {
+			match(env::var("DEV_DATABASE_URI"), env::var("DEV_LISTENING_ADDRESS"), env::var("DEV_LISTENING_PORT")) {
+				(Ok(d_d_uri), Ok(d_l_address), Ok(d_l_port)) => {
+					(database_uri, listening_address, listening_port) = (d_d_uri, d_l_address, d_l_port);
+				},
+				_ => {
+					panic!("One of the environment variables are not set");
+				}
+			};
+		},
+		Ok(state) if state.to_string() == "PROD"=> {
+			match(env::var("PROD_DATABASE_URI"), env::var("PROD_LISTENING_ADDRESS"), env::var("PROD_LISTENING_PORT")) {
+				(Ok(p_d_uri), Ok(p_l_address), Ok(p_l_port)) => {
+					(database_uri, listening_address, listening_port) = (p_d_uri, p_l_address, p_l_port);
+				},
+				_ => {
+					panic!("One of the environment variables are not set");
+				}
+			};
+		},
+		_ => {
+			panic!("One of the environment variables are not set");
 		}
-		
-	}else{
-		warn!("Connection string variables are invalid! Exiting program");
-	}
+ 	}
 	
     return Ok(())
 }
 
-
-async fn listen(mut stream:TcpStream)->Result<(), Box<dyn std::error::Error>>{
-	let mut buffer = [0; 1024];
-	let _bytes_read = stream.read(&mut buffer).await?;
-	
-	//calculating eaders
-	let mut headers = [httparse::EMPTY_HEADER; 64];
-	let mut req = httparse::Request::new(&mut headers);
-	let parse_result = req.parse(&buffer).unwrap();
-	let byte_offset = match parse_result {
-		httparse::Status::Complete(offset) => Ok(offset),
-		httparse::Status::Partial => {
-			Err(()) // would be nice if wecould create a response already and return an error with a malformed request
-		}
-    };
-	let content_length = req.headers.iter().find(|h| h.name == "Content-Length")
-		.and_then(|h| std::str::from_utf8(h.value).ok())
-		.and_then(|v| v.parse::<usize>().ok()).unwrap_or(0);
-
-	let body: &[u8] = &buffer[ byte_offset.unwrap()..(byte_offset.unwrap() + content_length)];
-	let body_str: std::borrow::Cow<str> = String::from_utf8_lossy(body);
-	
-	let req_headers: &mut [httparse::Header<'_>] = req.headers;
-
-	
-	let response_builder = Response::builder();
-	
-	let response_body = b"Hello world";
-	let constructed_response = response_builder.status(200)
-		.header("content-Length", response_body.len())
-		.header("Content-Type", "text/html")
-		.body(response_body).unwrap();
-	let (parts, body) = constructed_response.into_parts();
-	
-	let mut response_line = format!("{:#?} {}", parts.version, parts.status);
-	parts.headers.iter().for_each(
-		|(header_name, header_value)|{
-			response_line = response_line.clone() + "\r\n"+ &format!("{}:{}", header_name.as_str(), header_value.to_str().unwrap());
-		}		 
-	);
-	response_line = format!("{}\r\n\r\n{}", response_line,String::from_utf8(body.to_vec()).unwrap());
-	
-	println!("{}",response_line);
-	let _stream_write = stream.write_all(response_line.as_bytes()).await;
-	let _stream_flush = stream.flush().await;
-	return Ok(())
-	
-}
