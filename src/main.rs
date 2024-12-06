0045bf49547b0e9d44052d1d44844fb653ac31f3
Johnson <johnsonjaurigue2@gmail.com>
2024-12-06 20:11:58 +0800

    feat: added env database_name

--- src/main.rs
+++ src/main.rs
@@ -14,15 +14,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>>{
 	dotenv().ok();
 	
 	let mut database_uri = String::new();
+	let mut database_name = String::new();
 	let mut listening_address = String::new();
 	let mut listening_port = String::new();
 	
 	match env::var("STATE") {
 		
 		Ok(state) if state.to_string() == "DEV"=> {
-			match(env::var("DEV_DATABASE_URI"), env::var("DEV_LISTENING_ADDRESS"), env::var("DEV_LISTENING_PORT")) {
-				(Ok(d_d_uri), Ok(d_l_address), Ok(d_l_port)) => {
-					(database_uri, listening_address, listening_port) = (d_d_uri, d_l_address, d_l_port);
+			match(env::var("DEV_DATABASE_URI"), env::var("DEV_DATABASE_NAME"), env::var("DEV_LISTENING_ADDRESS"), env::var("DEV_LISTENING_PORT")) {
+				(Ok(d_d_uri), Ok(d_d_name), Ok(d_l_address), Ok(d_l_port)) => {
+					(database_uri, database_name, listening_address, listening_port) = (d_d_uri, d_d_name, d_l_address, d_l_port);
 				},
 				_ => {
 					panic!("One of the environment variables are not set");
@@ -30,9 +31,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>>{
 			};
 		},
 		Ok(state) if state.to_string() == "PROD"=> {
-			match(env::var("PROD_DATABASE_URI"), env::var("PROD_LISTENING_ADDRESS"), env::var("PROD_LISTENING_PORT")) {
-				(Ok(p_d_uri), Ok(p_l_address), Ok(p_l_port)) => {
-					(database_uri, listening_address, listening_port) = (p_d_uri, p_l_address, p_l_port);
+			match(env::var("PROD_DATABASE_URI"), env::var("PROD_DATABASE_NAME"), env::var("PROD_LISTENING_ADDRESS"), env::var("PROD_LISTENING_PORT")) {
+				(Ok(p_d_uri), Ok(p_d_name), Ok(p_l_address), Ok(p_l_port)) => {
+					(database_uri, database_name, listening_address, listening_port) = (p_d_uri, p_d_name, p_l_address, p_l_port);
 				},
 				_ => {
 					panic!("One of the environment variables are not set");
