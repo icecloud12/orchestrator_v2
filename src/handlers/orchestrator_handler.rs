4e61d3634287b61c4189d3922c931217b43bf038
Johnson <johnsonjaurigue2@gmail.com>
2024-12-07 13:03:24 +0800

    feat: database query for resolving multiple route matches

--- src/handlers/orchestrator_handler.rs
+++ src/handlers/orchestrator_handler.rs
@@ -11,20 +11,32 @@ pub async fn route_to_service_handler (request:Request, tcp_stream: TcpStream) -
 	println!("herer");
 	let uri_string = request.path.clone();
 	println!("uri_string: {}",uri_string);
-	let mut route_collection = MongoCollections::Routes.as_collection::<ServiceRoute>().find(doc! {
-		"$expr": {
-			"$eq": [
-				{
-					"$indexOfBytes": [
-						uri_string.clone(),
-						"$address"
-						
-					]
-				},
-				0
-			]
+	let pipeline = vec![
+		doc!{
+			"$match": {
+			"$expr": {
+				"$eq": [
+					{
+						"$indexOfBytes": [
+							uri_string.clone(),
+							"$address"
+							
+						]
+					},
+					0
+				]
+			}
+		}},
+		doc!{
+			"$sort": {
+				"segments": -1
+			}
+		},
+		doc!{
+			"$limit": 1
 		}
-	}).await?;
+	];
+	let mut route_collection = MongoCollections::Routes.as_collection::<ServiceRoute>().aggregate(pipeline).await?;
 	while route_collection.advance().await? {
 		println!("{:#?}", route_collection.deserialize_current())
 	}
--- src/models/service_route_model.rs
+++ src/models/service_route_model.rs
@@ -1,10 +1,12 @@
 use mongodb::bson::oid::ObjectId;
 use serde::{Deserialize, Serialize};
+use serde_json::Number;
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct ServiceRoute {
 	_id: ObjectId,
 	mongo_image: ObjectId, // a reference to the image,
 	address: String, //prefix of the service
-	exposed_port: String// port where the service within the container would listen
+	exposed_port: String,// port where the service within the container would listen,
+	segments: usize
 }
