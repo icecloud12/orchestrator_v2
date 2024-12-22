f1656deda49f56a96df7579304f384ab0ff6c76b
Johnson <johnsonjaurigue2@gmail.com>
2024-12-22 15:13:54 +0800

    feat: now tries to forward request after acknowledgement of container readyness

--- src/handlers/route_to_service_handler.rs
+++ src/handlers/route_to_service_handler.rs
@@ -4,126 +4,198 @@ use custom_tcp_listener::models::{router::response_to_bytes, types::Request};
 use http::StatusCode;
 use tokio::{io::AsyncWriteExt, net::TcpStream, sync::oneshot::error};
 
-use crate::{controllers::{load_balancer_controller::{get_or_init_load_balancer, ELoadBalancerMode, AWAITED_CONTAINERS, LOADBALANCERS}, route_controller::route_resolver}, models::{service_load_balancer, service_route_model::ServiceRoute}, utils::orchestrator_utils::{return_404, return_500, return_503}};
+use crate::{
+    controllers::{
+        load_balancer_controller::{
+            get_or_init_load_balancer, ELoadBalancerMode, AWAITED_CONTAINERS, LOADBALANCERS,
+        },
+        route_controller::route_resolver,
+    },
+    models::{
+        service_load_balancer, service_load_balancer_container_junction_model,
+        service_route_model::ServiceRoute,
+    },
+    utils::orchestrator_utils::{return_404, return_500, return_503},
+};
 
+pub async fn route_to_service_handler(
+    request: Request,
+    mut tcp_stream: TcpStream,
+) -> Result<(), Box<dyn Error>> {
+    let resolved_service = route_resolver(request.path.clone()).await;
+    match resolved_service {
+        Ok(t1) => {
+            match t1 {
+                Some(t2) => {
+                    let ServiceRoute {
+                        mongo_image,
+                        address,
+                        exposed_port,
+                        ..
+                    } = t2;
+                    let (lb_key, new_lb) =
+                        get_or_init_load_balancer(mongo_image, address, exposed_port)
+                            .await
+                            .unwrap();
+                    let lb_mutex = LOADBALANCERS.get().unwrap();
+                    let mut lb_hm = lb_mutex.lock().await;
+                    let lb = lb_hm.get_mut(&lb_key).unwrap();
+                    println!("{:#?}", &lb);
+                    match lb.mode {
+                        ELoadBalancerMode::FORWARD => {
+                            let next_container_result = lb.next_container().await;
+                            match next_container_result {
+                                Ok(container_public_port) => {
+                                    println!("next-container-succes");
+                                    let client_builder = reqwest::ClientBuilder::new();
+                                    let client = client_builder
+                                        .danger_accept_invalid_certs(true)
+                                        .build()
+                                        .unwrap();
+                                    let url = format!(
+                                        "https://localhost:{}{}",
+                                        container_public_port, request.path
+                                    );
 
-pub async fn route_to_service_handler (request:Request, mut tcp_stream: TcpStream) -> Result<(), Box<dyn Error>> {
-	let resolved_service = route_resolver(request.path.clone()).await;
-	match resolved_service {
-		Ok(t1) => {
-			match t1 {
-				Some(t2) => {
-					let ServiceRoute {mongo_image, address, exposed_port, ..} = t2;
-					let (lb_key, new_lb) = get_or_init_load_balancer(mongo_image,address,exposed_port).await.unwrap();
-					let lb_mutex = LOADBALANCERS.get().unwrap();
-					let mut lb_hm = lb_mutex.lock().await;
-					let lb= lb_hm.get_mut(&lb_key).unwrap();
-					println!("{:#?}", &lb);
-					match lb.mode {
-						ELoadBalancerMode::FORWARD => {
-							let next_container_result = lb.next_container().await;
-							match next_container_result {
-								Ok(container_public_port) => {
-									println!("next-container-succes");
-									let client_builder = reqwest::ClientBuilder::new();
-									let client = client_builder.danger_accept_invalid_certs(true).build().unwrap();
-									let url = format!("https://localhost:{}{}", container_public_port, request.path);
-									
-									let send_request_result: Result<reqwest::Response, reqwest::Error> = client.request(reqwest::Method::from_str(request.method.as_str()).unwrap(), url).headers(request.headers).body(request.body).send().await;
-									match send_request_result {
-										Ok(response) => {
-											let response_bytes = response.bytes().await.unwrap();
-											let _write_result = tcp_stream.write_all(&response_bytes).await;
-											let _flush_result = tcp_stream.flush().await;
-										},
-										Err(_) => { //errors concerning the connection towards the address
-											return_503(tcp_stream).await;
-										}
-									}
-								},
-								Err(_) => { //cannot next as it is empty
-									//create the container here
-									println!("next-container-fail");
-									match lb.create_container().await {
-										Ok(new_container) => {
-											println!("create-container-success");
-											//created  successfully	
-											match new_container.start_container().await {
-												Ok(_) => {
-													lb.mode = ELoadBalancerMode::QUEUE;
-													lb.queue_stream(tcp_stream);
-													lb.queue_container(new_container);
-												},
-												Err(docker_start_error) => {
-													return_500(tcp_stream, docker_start_error).await;
-												}
-											}
-										},
-										Err(docker_create_error) => {
-											println!("create-container-fail");
-											return_500(tcp_stream, docker_create_error).await;
-										},
-									}
-									
-									
-								},
-							};
-						},
-						ELoadBalancerMode::QUEUE => {
-							// lb.queue_stream(tcp_stream);
-							if new_lb {
-								match lb.create_container().await {
-									Ok(new_container)=> {
-										match new_container.start_container().await {
-											Ok(_container_start_success) => {
-												lb.queue_stream(tcp_stream);
-												lb.queue_container(new_container);
-											},
-											Err(container_start_error) => {
-												return_500(tcp_stream, container_start_error).await;
-											}
-										}
-									},
-									Err(container_create_error)=> {
-										return_500(tcp_stream, container_create_error).await;
-									}
-								}
-							}else{
-								lb.queue_stream(tcp_stream);
-							}
-						},
-					}
-				},
-				None => {
-					return_404(tcp_stream).await;
-				},
-			}
-		},
-		Err(err) => {
-			return_500(tcp_stream, err).await;
-		},
-	};
-	Ok(())	
+                                    let send_request_result: Result<
+                                        reqwest::Response,
+                                        reqwest::Error,
+                                    > = client
+                                        .request(
+                                            reqwest::Method::from_str(request.method.as_str())
+                                                .unwrap(),
+                                            url,
+                                        )
+                                        .headers(request.headers)
+                                        .body(request.body)
+                                        .send()
+                                        .await;
+                                    match send_request_result {
+                                        Ok(response) => {
+                                            let response_bytes = response.bytes().await.unwrap();
+                                            let _write_result =
+                                                tcp_stream.write_all(&response_bytes).await;
+                                            let _flush_result = tcp_stream.flush().await;
+                                        }
+                                        Err(_) => {
+                                            //errors concerning the connection towards the address
+                                            return_503(tcp_stream).await;
+                                        }
+                                    }
+                                }
+                                Err(_) => {
+                                    //cannot next as it is empty
+                                    //create the container here
+                                    println!("next-container-fail");
+                                    match lb.create_container().await {
+                                        Ok(new_container) => {
+                                            println!("create-container-success");
+                                            //created  successfully
+                                            match new_container.start_container().await {
+                                                Ok(_) => {
+                                                    lb.mode = ELoadBalancerMode::QUEUE;
+                                                    lb.queue_stream(tcp_stream);
+                                                    lb.queue_container(new_container).await;
+                                                }
+                                                Err(docker_start_error) => {
+                                                    return_500(tcp_stream, docker_start_error)
+                                                        .await;
+                                                }
+                                            }
+                                        }
+                                        Err(docker_create_error) => {
+                                            println!("create-container-fail");
+                                            return_500(tcp_stream, docker_create_error).await;
+                                        }
+                                    }
+                                }
+                            };
+                        }
+                        ELoadBalancerMode::QUEUE => {
+                            if new_lb {
+                                match lb.create_container().await {
+                                    Ok(new_container) => {
+                                        match new_container.start_container().await {
+                                            Ok(_container_start_success) => {
+                                                println!("queueing container");
+                                                lb.queue_stream(tcp_stream);
+                                                lb.queue_container(new_container).await;
+                                            }
+                                            Err(container_start_error) => {
+                                                return_500(tcp_stream, container_start_error).await;
+                                            }
+                                        }
+                                    }
+                                    Err(container_create_error) => {
+                                        return_500(tcp_stream, container_create_error).await;
+                                    }
+                                }
+                            } else {
+                                lb.queue_stream(tcp_stream);
+                            }
+                        }
+                    }
+                }
+                None => {
+                    return_404(tcp_stream).await;
+                }
+            }
+        }
+        Err(err) => {
+            return_500(tcp_stream, err).await;
+        }
+    };
+    Ok(())
 }
 
-pub async fn container_ready(request:Request, mut tcp_stream: TcpStream) -> Result<(), Box<dyn Error>> {
-	let params = request.parameters;
-	let uuid = params.get("uuid").unwrap();
-	let awaited_container_mutex = AWAITED_CONTAINERS.get().unwrap();
-	let awaited_containers = awaited_container_mutex.lock().await;
-	let load_balancer_key = awaited_containers.get(uuid).unwrap().clone();
-	drop(awaited_containers);
-	let load_balacer_mutex = LOADBALANCERS.get().unwrap();
-	let mut load_balancers = load_balacer_mutex.lock().await;
-	let service_load_balancer = load_balancers.get_mut(&load_balancer_key).unwrap();
-	let awaited_container_key_val_pair = service_load_balancer.awaited_containers.remove_entry(uuid).unwrap();
-	service_load_balancer.add_container(awaited_container_key_val_pair.1);
+pub async fn container_ready(
+    request: Request,
+    mut tcp_stream: TcpStream,
+) -> Result<(), Box<dyn Error>> {
+    let params = request.parameters;
+    let uuid = params.get("uuid").unwrap();
+    let awaited_container_mutex = AWAITED_CONTAINERS.get().unwrap();
+    let awaited_containers = awaited_container_mutex.lock().await;
+    println!("{:#?}", &awaited_containers);
+    let load_balancer_key = awaited_containers.get(uuid).unwrap().clone();
+    drop(awaited_containers);
+    let load_balacer_mutex = LOADBALANCERS.get().unwrap();
+    let mut load_balancers = load_balacer_mutex.lock().await;
+    let service_load_balancer = load_balancers.get_mut(&load_balancer_key).unwrap();
+    let awaited_container_key_val_pair = service_load_balancer
+        .awaited_containers
+        .remove_entry(uuid)
+        .unwrap();
+    let readied_container = awaited_container_key_val_pair.1;
+    let container_port = readied_container.public_port.clone();
+    service_load_balancer.add_container(readied_container);
 
-	//respond to the stream with an empty OK body
-	let empty_body: &[u8] = &Vec::new();
-	let response_builder = http::Response::builder().status(StatusCode::OK).body(empty_body).unwrap();
-	let response_bytes = response_to_bytes(response_builder);
-	let _write_result = tcp_stream.write_all(&response_bytes).await; //we dont care if it fails
-	let _flush_result = tcp_stream.flush().await;
-	Ok(())
+    //acknowledge container readyness
+    let empty_body: &[u8] = &Vec::new();
+    let response_builder = http::Response::builder()
+        .status(StatusCode::OK)
+        .body(empty_body)
+        .unwrap();
+    let response_bytes = response_to_bytes(response_builder);
+    let _write_result = tcp_stream.write_all(&response_bytes).await; //we dont care if it fails
+    let _flush_result = tcp_stream.flush().await;
+    //if the load balancer is queue then change it to forwarding
+    match service_load_balancer.mode {
+        ELoadBalancerMode::QUEUE => {
+            service_load_balancer.mode = ELoadBalancerMode::FORWARD;
+            service_load_balancer
+                .tcp_queue
+                .iter()
+                .for_each(|queued_tcp_stream| {
+                    //foward all queued stream to the newly made container
+                    //todo NEED TO STORE THE REQUEST INFO AS WELL
+                    queued_tcp_stream;
+                });
+        }
+        ELoadBalancerMode::FORWARD => {
+            //do nothing
+        }
+    };
+    service_load_balancer.tcp_queue = vec![];
+    Ok(())
 }
--- src/models/service_load_balancer.rs
+++ src/models/service_load_balancer.rs
@@ -4,11 +4,12 @@ use mongodb::bson::oid::ObjectId;
 use serde::{Deserialize, Serialize};
 use tokio::net::TcpStream;
 
-use crate::controllers::{container_controller, load_balancer_container_junction, load_balancer_controller::{ELoadBalancerBehavior, ELoadBalancerMode, AWAITED_CONTAINERS}};
+use crate::controllers::{
+    container_controller, load_balancer_container_junction,
+    load_balancer_controller::{ELoadBalancerBehavior, ELoadBalancerMode, AWAITED_CONTAINERS},
+};
 
 use super::service_container_models::ServiceContainer;
-
-
 
 #[derive(Debug)]
 //this struct represents the load balancer of the current program
@@ -17,25 +18,24 @@ pub struct ServiceLoadBalancer {
     pub docker_image_id: String,
     pub exposed_port: String,
     pub address: String,
-    pub head: usize,//current head pointer of the load_balancer
+    pub head: usize, //current head pointer of the load_balancer
     pub behavior: ELoadBalancerBehavior,
     pub mode: ELoadBalancerMode,
     pub containers: Vec<ServiceContainer>, //docker_container_id_instances
-    pub awaited_containers: HashMap<String, ServiceContainer>,//docker_containers not pushed to the active container vector
+    pub awaited_containers: HashMap<String, ServiceContainer>, //docker_containers not pushed to the active container vector
     pub validated: bool, //initially false to let the program know if the containers are checke,
-    pub tcp_queue: Vec<TcpStream> 
+    pub tcp_queue: Vec<TcpStream>,
 }
 
 impl ServiceLoadBalancer {
     ///the container array size differ depending on:
     /// 1. if it's newly created
     /// 2. the recorded load_balancer in the database have/have no entries
-    pub async fn next_container(&mut self) -> Result<usize, ()>
-    {
+    pub async fn next_container(&mut self) -> Result<usize, ()> {
         let containers = &self.containers;
-        
+
         let container_len = containers.len();
-        let ret: Result<usize, ()>  = if container_len == 0 {
+        let ret: Result<usize, ()> = if container_len == 0 {
             // let create_container_result = self.create_container().await;
             // //maybe create some logic before creating the container here for scalability logic
             // match create_container_result {
@@ -43,60 +43,67 @@ impl ServiceLoadBalancer {
             //     Err(err) => Err(err),
             // }
             Err(())
-        }else{//has container entries
+        } else {
+            //has container entries
             //move the head
-            let head =  &mut self.head;
+            let head = &mut self.head;
             *head = (*head + 1) % container_len;
-			let public_port = containers.get(*head).unwrap().public_port.clone();
+            let public_port = containers.get(*head).unwrap().public_port.clone();
             Ok(public_port)
         };
         ret
     }
 
-    pub async fn create_container(&mut self) -> Result<ServiceContainer, String>{
-        let col = container_controller::create_container(&self.docker_image_id, &self.exposed_port).await;
-        
-        match col {   
+    pub async fn create_container(&mut self) -> Result<ServiceContainer, String> {
+        let col =
+            container_controller::create_container(&self.docker_image_id, &self.exposed_port).await;
+
+        match col {
             Ok(service_container) => {
-                let mut lock  = AWAITED_CONTAINERS.get().unwrap().lock().await;
-                lock.insert(service_container.container_id.clone(), self.docker_image_id.clone());
+                let mut lock = AWAITED_CONTAINERS.get().unwrap().lock().await;
+                lock.insert(
+                    service_container.container_id.clone(),
+                    self.docker_image_id.clone(),
+                );
                 drop(lock);
-				let _ = load_balancer_container_junction::create(&self._id, &service_container._id).await;
+                let _ = load_balancer_container_junction::create(&self._id, &service_container._id)
+                    .await;
                 Ok(service_container)
-            },
+            }
             Err(err) => Err(err),
         }
     }
     pub fn queue_stream(&mut self, tcp_stream: TcpStream) {
         self.tcp_queue.push(tcp_stream);
     }
-	pub fn add_container(&mut self, container: ServiceContainer) {
-		let containers = &mut self.containers;
-		containers.push(container);
-	}
-    pub fn queue_container(&mut self, container: ServiceContainer) {
+    pub fn add_container(&mut self, container: ServiceContainer) {
+        let containers = &mut self.containers;
+        containers.push(container);
+    }
+    pub async fn queue_container(&mut self, container: ServiceContainer) {
+        let mut await_container_lock = AWAITED_CONTAINERS.get().unwrap().lock().await;
+        await_container_lock.insert(container.uuid.clone(), self.docker_image_id.clone());
         let awaited_containers = &mut self.awaited_containers;
         awaited_containers.insert(container.uuid.clone(), container);
     }
 }
-
 //this struct represents the data from mongodb
-//will be used to quickly restore the load-balancers from the old state just incase 
+//will be used to quickly restore the load-balancers from the old state just incase
 //if the orchestrator is closed due to user intervention but docker isn't
 //load from cache thingy-majig if that makes sense
 #[derive(Deserialize, Serialize, Debug)]
 pub struct LoadBalancerEntryAggregate {
-    pub _id: ObjectId, 
-    pub mongo_image_reference:ObjectId, //mongo_image_reference
+    pub _id: ObjectId,
+    pub mongo_image_reference: ObjectId, //mongo_image_reference
     pub head: usize,
     pub behavior: String,
-	pub containers: Vec<ServiceContainer>
+    pub containers: Vec<ServiceContainer>,
 }
 
 #[derive(Deserialize, Serialize)]
 pub struct LoadBalancerEntry {
-    pub _id: ObjectId, 
-    pub mongo_image_reference:ObjectId, //mongo_image_reference
+    pub _id: ObjectId,
+    pub mongo_image_reference: ObjectId, //mongo_image_reference
     pub head: usize,
     pub behavior: String,
 }
@@ -104,7 +111,7 @@ pub struct LoadBalancerEntry {
 ///struct for insert purposes only
 #[derive(Serialize)]
 pub struct LoadBalancerInsert {
-    pub mongo_image_reference:ObjectId, //mongo_image_reference
+    pub mongo_image_reference: ObjectId, //mongo_image_reference
     pub head: usize,
     pub behavior: String,
 }
